15f9a4b06fd64225d9ae3dbafe89e28dc02e489a
Niccolò Barbieri <75387092+Niconiki99@users.noreply.github.com>
2022-03-14 14:49:35 +0100

    First version
    File with only rule 30 working starting with already decided state

---
+++ 30rule.py
@@ -0,0 +1,46 @@
+def generate_state():
+    return ".00."
+
+
+def traduct(string):
+    rule30 = {"000": '.',
+          "00.": '.',
+          "0.0": '.',
+          "...": '.',
+          "0..": '0',
+          ".00": '0',
+          ".0.": '0',
+          "..0": '0',
+         }
+    return rule30[string]
+
+def evolve(state):
+    new_state=traduct(state[-1]+state[0]+state[1])
+    for i in range(len(state)-2):
+        new_state+=traduct(state[i:i+3])
+    new_state=new_state+traduct(state[-2]+state[-1]+state[1])
+    return new_state
+
+def simulation(nsteps):
+    initial_state = generate_state()
+    states_seq = [initial_state]
+    for i in range(nsteps):
+        old_state = states_seq[-1]
+        new_state = evolve(old_state)
+        states_seq.append(new_state)
+    return states_seq
+
+def test_evolve0():
+    assert evolve("0000")=="...."
+    
+def test_genType():
+    assert type(generate_state()) is str
+
+def test_trad():
+    assert traduct("000")=='.'
+
+def test_evolvelen():
+    assert len(evolve(generate_state()))==len(generate_state())
+
+risultati=simulation(10)
+for i in risultati: print(i)
