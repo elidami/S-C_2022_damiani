7917f8555e52de0a49f9ad27f9e523e63985ba90
Niconiki99 <niccolobarbieri99@gmail.com>
2022-03-14 18:45:41 +0100

    End of First example of Documentation

--- 30rule.py
+++ 30rule.py
@@ -3,12 +3,15 @@ import string
 
 
 def generate_state(leng):
-    return ''.join(random.choices('0'+'.', k=leng)) #Creates a random string of fixed lenght 
+    #Creates a random string of fixed lenght
+    #requires the lenght of the string e returns a random generated string
+    return ''.join(random.choices('0'+'.', k=leng)) 
 
 def traduct(string):
     #Traduction function, taking one number and the string to traduct, 
     #it can produce different rules, 
     #in particular rule 30, rule 90 rule 110 and rule 184
+    #requires as parameter the string to traduce and returns the character which is associated to that string
     rule30 = {"000": '.',
           "00.": '.',
           "0.0": '.',
@@ -21,6 +24,8 @@ def traduct(string):
     return rule30[string]
 
 def evolve(state):
+    #Controls evolution of motus, following the traduction function defined above;
+    #require as parameter the state which has to be evolved, and returns the new state, defined to be both strings
     new_state=traduct(state[-1]+state[0]+state[1])
     for i in range(len(state)-2):
         new_state+=traduct(state[i:i+3])
@@ -28,6 +33,10 @@ def evolve(state):
     return new_state
 
 def simulation(nsteps,n):
+    #Main simulation controll, 
+    #returns a list of the states which have been defined over the process, 
+    #requires as parameters the number of iterations which have to be calculated, 
+    #the number of the rule which we want to follow and the lenght of the string we want to describe 
     initial_state = generate_state(12)
     states_seq = [initial_state]
     for i in range(nsteps):
