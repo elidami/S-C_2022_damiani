3d39fcfb24ab7c52d4900e258173272aebb167f4
Niconiki99 <niccolobarbieri99@gmail.com>
2022-03-14 19:20:45 +0100

    All rules support implemented

--- 30rule.py
+++ 30rule.py
@@ -7,41 +7,46 @@ def generate_state(leng):
     #requires the lenght of the string e returns a random generated string
     return ''.join(random.choices('0'+'.', k=leng)) 
 
-def traduct(string):
+def traduct(string,rule_select):
     #Traduction function, taking one number and the string to traduct, 
     #it can produce different rules, 
     #in particular rule 30, rule 90 rule 110 and rule 184
-    #requires as parameter the string to traduce and returns the character which is associated to that string
-    rule30 = {"000": '.',
-          "00.": '.',
-          "0.0": '.',
-          "...": '.',
-          "0..": '0',
-          ".00": '0',
-          ".0.": '0',
-          "..0": '0',
-         }
-    return rule30[string]
+    #requires as parameter the string to traduce, and an integer associated with the rule to follow
+    #and returns the character which is associated to that string
+    RULES = {30: {"...": '0', "..0": '0', ".0.": '0', "000": '0',
+              ".00": '.', "0..": '.', "0.0": '.', "00.": '.'},
 
-def evolve(state):
+         90: {"...": "0", "..0": ".", ".0.": "0", ".00": ".",
+              "0..": ".", "0.0": "0", "00.": ".", "000": "0"},
+
+         110: {"...": '0', "..0": '.', ".0.": '.', ".00": '0',
+               "0..": '.', "0.0": '.', "00.": '.', "000": '0'},
+
+         184: {"...": ".", "..0": "0", ".0.": ".", ".00": ".",
+               "0..": ".", "0.0": "0", "00.": "0", "000": "0"}
+         }
+    return RULES[rule_select][string]
+
+def evolve(state,rule_select):
     #Controls evolution of motus, following the traduction function defined above;
-    #require as parameter the state which has to be evolved, and returns the new state, defined to be both strings
-    new_state=traduct(state[-1]+state[0]+state[1])
+    #require as parameter the state which has to be evolved and an integer to select the rule,
+    #and returns the new state, defined to be both strings
+    new_state=traduct(state[-1]+state[0]+state[1],rule_select)
     for i in range(len(state)-2):
-        new_state+=traduct(state[i:i+3])
-    new_state=new_state+traduct(state[-2]+state[-1]+state[0])
+        new_state+=traduct(state[i:i+3],rule_select)
+    new_state=new_state+traduct(state[-2]+state[-1]+state[0],rule_select)
     return new_state
 
-def simulation(nsteps,n):
+def simulation(nsteps,string_len=12,rule_select=30):
     #Main simulation controll, 
     #returns a list of the states which have been defined over the process, 
     #requires as parameters the number of iterations which have to be calculated, 
     #the number of the rule which we want to follow and the lenght of the string we want to describe 
-    initial_state = generate_state(12)
+    initial_state = generate_state(string_len)
     states_seq = [initial_state]
     for i in range(nsteps):
         old_state = states_seq[-1]
-        new_state = evolve(old_state)
+        new_state = evolve(old_state,rule_select)
         states_seq.append(new_state)
     return states_seq
 
@@ -57,5 +62,5 @@ def test_trad():
 def test_evolvelen():
     assert len(evolve(generate_state(12)))==len(generate_state(12))
 
-risultati=simulation(10)
+risultati=simulation(10,12,30)
 for i in risultati: print(i)
